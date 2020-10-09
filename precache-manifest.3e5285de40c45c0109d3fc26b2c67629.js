f1dfbd1f10746b872d2e6757ac292dfc190f0481
ISvirko <ISvirko@yahoo.com>
2020-10-10 02:21:53 +0300

    Updates

---
+++ precache-manifest.3e5285de40c45c0109d3fc26b2c67629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41244d854fbb9147922752ba442a8d2a",
+    "url": "/hotdogs-react/index.html"
+  },
+  {
+    "revision": "e6095738db1c43a17477",
+    "url": "/hotdogs-react/static/css/main.f49fa8cc.chunk.css"
+  },
+  {
+    "revision": "01da394147a0cebb18f5",
+    "url": "/hotdogs-react/static/js/2.067a25c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hotdogs-react/static/js/2.067a25c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6095738db1c43a17477",
+    "url": "/hotdogs-react/static/js/main.b6918fec.chunk.js"
+  },
+  {
+    "revision": "fcbb59c86297b37dbdcc",
+    "url": "/hotdogs-react/static/js/runtime-main.eb7191de.js"
+  },
+  {
+    "revision": "3bc1d15920c2aa77598176f7b472bda1",
+    "url": "/hotdogs-react/static/media/hotdog-icon.3bc1d159.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8850296ef0f07c0b7306bf6eef193461.js"
+  "/hotdogs-react/precache-manifest.3e5285de40c45c0109d3fc26b2c67629.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hotdogs-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
